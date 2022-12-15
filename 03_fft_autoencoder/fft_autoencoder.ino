267f99e7a2ae4398bd78cf1fc64560c3b9add960
takao@SORACOM <ide.takao@gmail.com>
2022-12-15 20:39:09 +0900

    LTEsetupを実装

--- 03_fft_autoencoder/fft_autoencoder.ino
+++ 03_fft_autoencoder/fft_autoencoder.ino
@@ -25,7 +25,44 @@ SDClass SD;
 #include <DNNRT.h>
 DNNRT dnnrt;
 
+// libraries
+#include <LTE.h>
+
 #define FFT_LEN 1024
+
+// APN name
+#define APP_LTE_APN "soracom.io" // replace your APN
+
+/* APN authentication settings
+ * Ignore these parameters when setting LTE_NET_AUTHTYPE_NONE.
+ */
+
+#define APP_LTE_USER_NAME "sora" // replace with your username
+#define APP_LTE_PASSWORD  "sora" // replace with your password
+
+// APN IP type
+#define APP_LTE_IP_TYPE (LTE_NET_IPTYPE_V4V6) // IP : IPv4v6
+// #define APP_LTE_IP_TYPE (LTE_NET_IPTYPE_V4) // IP : IPv4
+// #define APP_LTE_IP_TYPE (LTE_NET_IPTYPE_V6) // IP : IPv6
+
+// APN authentication type
+#define APP_LTE_AUTH_TYPE (LTE_NET_AUTHTYPE_CHAP) // Authentication : CHAP
+// #define APP_LTE_AUTH_TYPE (LTE_NET_AUTHTYPE_PAP) // Authentication : PAP
+// #define APP_LTE_AUTH_TYPE (LTE_NET_AUTHTYPE_NONE) // Authentication : NONE
+
+/* RAT to use
+ * Refer to the cellular carriers information
+ * to find out which RAT your SIM supports.
+ * The RAT set on the modem can be checked with LTEModemVerification::getRAT().
+ */
+
+#define APP_LTE_RAT (LTE_NET_RAT_CATM) // RAT : LTE-M (LTE Cat-M1)
+// #define APP_LTE_RAT (LTE_NET_RAT_NBIOT) // RAT : NB-IoT
+
+// initialize the library instance
+LTE        lteAccess;
+LTEScanner scannerNetworks;
+
 
 // モノラル、1024サンプルでFFTを初期化
 FFTClass<AS_CHANNEL_MONO, FFT_LEN> FFT;
@@ -48,11 +85,60 @@ void avgFilter(float dst[FFT_LEN]) {
   ++g_counter;
 }
 
+void ltesetup() {
+  char apn[LTE_NET_APN_MAXLEN] = APP_LTE_APN;
+  LTENetworkAuthType authtype = APP_LTE_AUTH_TYPE;
+  char user_name[LTE_NET_USER_MAXLEN] = APP_LTE_USER_NAME;
+  char password[LTE_NET_PASSWORD_MAXLEN] = APP_LTE_PASSWORD;
+
+  Serial.println("LTE networks scanner");
+
+  while (true) {
+
+    /* Power on the modem and Enable the radio function. */
+
+    if (lteAccess.begin() != LTE_SEARCHING) {
+      Serial.println("Could not transition to LTE_SEARCHING.");
+      Serial.println("Please check the status of the LTE board.");
+      for (;;) {
+        sleep(1);
+      }
+    }
+
+    /* The connection process to the APN will start.
+     * If the synchronous parameter is false,
+     * the return value will be returned when the connection process is started.
+     */
+    if (lteAccess.attach(APP_LTE_RAT,
+                         apn,
+                         user_name,
+                         password,
+                         authtype,
+                         APP_LTE_IP_TYPE) == LTE_READY) {
+      Serial.println("attach succeeded.");
+      break;
+    }
+
+    /* If the following logs occur frequently, one of the following might be a cause:
+     * - APN settings are incorrect
+     * - SIM is not inserted correctly
+     * - If you have specified LTE_NET_RAT_NBIOT for APP_LTE_RAT,
+     *   your LTE board may not support it.
+     * - Rejected from LTE network
+     */
+    Serial.println("An error has occurred. Shutdown and retry the network attach process after 1 second.");
+    lteAccess.shutdown();
+    sleep(1);
+  }
+}
+
 
 void setup() {
   Serial.begin(115200);
   // SDカードの入力を待つ
   while (!SD.begin()) {Serial.println("Insert SD card");};
+
+  ltesetup();
 
   Serial.println("Initialize DNNRT");
   // SDカード上にある学習済モデルを読み込む
@@ -78,14 +164,15 @@ void setup() {
   // DSPコーデックの場所の指定 (SDカード上のBINディレクトリ)、
   // サンプリグレート 48000Hz、モノラル入力  
   int err = theAudio->initRecorder(AS_CODECTYPE_PCM,
-      "/mnt/sd0/BIN", AS_SAMPLINGRATE_48000 ,AS_CHANNEL_MONO);                             
-  if (err != AUDIOLIB_ECODE_OK) {
+    "/mnt/sd0/BIN", AS_SAMPLINGRATE_48000 ,AS_CHANNEL_MONO);                             
+    if (err != AUDIOLIB_ECODE_OK) {
     Serial.println("Recorder initialize error");
     while(1);
   }
 
   Serial.println("Start Recorder");
   theAudio->startRecorder(); // 録音開始
+
 }
 
 
@@ -157,8 +244,9 @@ void loop(){
   // 閾値でOK/NGを判定
   bool bNG = false;
   avg_err > threshold ? bNG = true : bNG = false;
-  if (bNG) Serial.println("Fault on the pipe");
+  if (bNG) Serial.println("Fault on the machine");
 
+　
 
 
 }
